9da371d5a239dd802faeed7f8075fade8ab2c389
Arsenii78 <hypercactissupercube@gmail.com>
2025-04-26 18:20:14 +0300

    Third commit

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -6,8 +6,12 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Домашняя страница',)
+    return render_template('buttons1.html', title='Домашняя страница',)
 
+
+@app.route('/history')
+def history():
+    pass
 
 if __name__ == '__main__':
     app.run()
