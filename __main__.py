2a205c4d408ee50529f8b3736263e794ed6d30f5
Arsenii78 <hypercactissupercube@gmail.com>
2025-04-27 21:17:25 +0300

    11 commit

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
+from forms.user import RegisterForm
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -8,8 +10,30 @@ def main():
     db_session.global_init("db/blogs.db")
     app.run()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return redirect('/base')
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/base')
+    return render_template('register.html', title='Регистрация', form=form)
+
+@app.route('/base', methods=['GET', 'POST'])
 def index():
     return render_template('buttons1.html')
 
