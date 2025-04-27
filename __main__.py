10bc35b302272d400c333d71fa8bb0441841c802
Arsenii78 <hypercactissupercube@gmail.com>
2025-04-27 14:56:27 +0300

    9 commit

--- __main__.py
+++ __main__.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from data import db_session
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run()
 
 @app.route('/')
 @app.route('/index')
@@ -12,5 +18,7 @@ def index():
 def history():
     return render_template('buttons_history.html')
 
+
+
 if __name__ == '__main__':
-    app.run()
+    main()
